2dbbb0ed98512abc077602d58bbb81631377907f
“Lukasz <lukasz.glowacz@glowacz.se>
2025-01-08 18:32:28 +0100

    Add February 2025 heading and adjust animations

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const logo = document.querySelector('.logo');
 const h4 = document.querySelector('h4');
 const toggleButton = document.getElementById('toggleAnimation');
 const icon = toggleButton.querySelector('i');
+const h1 = document.querySelector('h1');
 
 let isAnimationRunning = true;
 
@@ -10,12 +11,14 @@ toggleButton.addEventListener('click', () => {
         // Pause all animations
         logo.style.animationPlayState = 'paused';
         h4.style.animationPlayState = 'paused';
+        h1.style.animationPlayState = 'paused';
         icon.classList.remove('fa-pause');
         icon.classList.add('fa-play');
     } else {
         // Resume all animations
         logo.style.animationPlayState = 'running';
         h4.style.animationPlayState = 'running';
+        h1.style.animationPlayState = 'running';
         icon.classList.remove('fa-play');
         icon.classList.add('fa-pause');
     }
