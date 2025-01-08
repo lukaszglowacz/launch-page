8dbd98bf4ce771211ff4b16a3fb955cc45b284a6
“Lukasz <lukasz.glowacz@glowacz.se>
2025-01-08 18:11:44 +0100

    Update animations and styles

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const logo = document.querySelector('.logo');
 const h4 = document.querySelector('h4');
+const h1 = document.querySelector('.coming-soon');
 const toggleButton = document.getElementById('toggleAnimation');
 const icon = toggleButton.querySelector('i');
 
 let isAnimationRunning = true;
 
 toggleButton.addEventListener('click', () => {
-    if (isAnimationRunning) {
-        // Pause all animations
-        logo.style.animationPlayState = 'paused';
-        h4.style.animationPlayState = 'paused';
-        icon.classList.remove('fa-pause');
-        icon.classList.add('fa-play');
-    } else {
-        // Resume all animations
-        logo.style.animationPlayState = 'running';
-        h4.style.animationPlayState = 'running';
-        icon.classList.remove('fa-play');
-        icon.classList.add('fa-pause');
-    }
-    isAnimationRunning = !isAnimationRunning;
+  if (isAnimationRunning) {
+    // Pause animations
+    logo.style.animationPlayState = 'paused';
+    h4.style.animationPlayState = 'paused';
+    h1.style.animationPlayState = 'paused';
+    icon.classList.replace('fa-pause', 'fa-play');
+  } else {
+    // Resume animations
+    logo.style.animationPlayState = 'running';
+    h4.style.animationPlayState = 'running';
+    h1.style.animationPlayState = 'running';
+    icon.classList.replace('fa-play', 'fa-pause');
+  }
+  isAnimationRunning = !isAnimationRunning;
 });
