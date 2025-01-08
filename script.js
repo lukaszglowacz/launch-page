4a94a8816aee4303798c0d71f629e2fcb55d721d
“Lukasz <lukasz.glowacz@glowacz.se>
2025-01-08 01:24:58 +0100

    Update logo and text in index.html, and adjust animation timing in style.css

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const logo = document.querySelector('.logo');
-const h1 = document.querySelector('h1');
 const h4 = document.querySelector('h4');
 const toggleButton = document.getElementById('toggleAnimation');
 const icon = toggleButton.querySelector('i');
@@ -10,14 +9,12 @@ toggleButton.addEventListener('click', () => {
     if (isAnimationRunning) {
         // Pause all animations
         logo.style.animationPlayState = 'paused';
-        h1.style.animationPlayState = 'paused';
         h4.style.animationPlayState = 'paused';
         icon.classList.remove('fa-pause');
         icon.classList.add('fa-play');
     } else {
         // Resume all animations
         logo.style.animationPlayState = 'running';
-        h1.style.animationPlayState = 'running';
         h4.style.animationPlayState = 'running';
         icon.classList.remove('fa-play');
         icon.classList.add('fa-pause');
